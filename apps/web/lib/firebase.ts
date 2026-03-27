1667f0296f6d658a966b8f2b2f844ac1c71a9e88
WERA Admin <admin@wera.com>
2026-03-27 18:12:42 +0530

    fix(web): complete firebase client-side configuration

--- apps/web/lib/firebase.ts
+++ apps/web/lib/firebase.ts
@@ -6,8 +6,8 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: "placeholder",
-  appId: "placeholder",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
